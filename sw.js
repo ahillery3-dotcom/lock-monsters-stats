4a7f4c46ca31511768acb36e26f231f533ced5a2
ahillery3-dotcom <ahillery3@gmail.com>
2026-08-07 17:55:48 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,1 @@
-const CACHE='lockmonsters-pwa-v10-rc1';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(
-  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
-));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
-  e.respondWith(
-    fetch(e.request).then(resp=>{
-      const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp
-    }).catch(()=>caches.match(e.request).then(x=>x||caches.match('./index.html')))
-  )
-});
+const C='lm-v11';const A=['./','./index.html','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{let x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request).then(x=>x||caches.match('./index.html'))))});
